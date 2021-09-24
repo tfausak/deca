eb20922c9cdd6237bd961452f9696b253ce5274d
Taylor Fausak <taylor@fausak.me>
2021-09-24 11:27:31 +0000

    Use functions to implement methods

--- src/lib/Deca.hs
+++ src/lib/Deca.hs
@@ -5,14 +5,23 @@ data Deca significand exponent
     deriving (Eq, Show)
 
 instance (Integral s, Num e, Ord e) => Num (Deca s e) where
-    abs (Deca s e) = Deca (abs s) e
-    fromInteger s = normalize $ Deca (fromInteger s) 0
-    negate (Deca s e) = Deca (negate s) e
-    signum (Deca s _) = Deca (signum s) 0
-    x + y =
-        let (Deca s1 e1, Deca s2 _) = denormalize x y
-        in normalize $ Deca (s1 + s2) e1
-    Deca s1 e1 * Deca s2 e2 = normalize $ Deca (s1 * s2) (e1 + e2)
+    abs = Deca.abs
+    fromInteger = Deca.fromInteger
+    negate = Deca.negate
+    signum = Deca.signum
+    (+) = add
+    (*) = multiply
+
+abs :: Num s => Deca s e -> Deca s e
+abs (Deca s e) = Deca (Prelude.abs s) e
+
+add :: (Integral s, Num e, Ord e) => Deca s e -> Deca s e -> Deca s e
+add x y =
+    let (Deca s1 e1, Deca s2 _) = denormalize x y
+    in normalize $ Deca (s1 + s2) e1
+
+deca :: (Integral s, Num e) => s -> e -> Deca s e
+deca s e = normalize $ Deca s e
 
 denormalize :: (Num s, Num e, Ord e) => Deca s e -> Deca s e -> (Deca s e, Deca s e)
 denormalize (Deca s1 e1) (Deca s2 e2) = case compare e1 e2 of
@@ -20,8 +29,14 @@ denormalize (Deca s1 e1) (Deca s2 e2) = case compare e1 e2 of
     EQ -> (Deca s1 e1, Deca s2 e2)
     GT -> denormalize (Deca (s1 * 10) (e1 - 1)) (Deca s2 e2)
 
-deca :: (Integral s, Num e) => s -> e -> Deca s e
-deca s e = normalize $ Deca s e
+fromInteger :: (Integral s, Num e) => Integer -> Deca s e
+fromInteger s = normalize $ Deca (Prelude.fromInteger s) 0
+
+multiply :: (Integral s, Num e) => Deca s e -> Deca s e -> Deca s e
+multiply (Deca s1 e1) (Deca s2 e2) = normalize $ Deca (s1 * s2) (e1 + e2)
+
+negate :: Num s => Deca s e -> Deca s e
+negate (Deca s e) = Deca (Prelude.negate s) e
 
 normalize :: (Integral s, Num e) => Deca s e -> Deca s e
 normalize (Deca s e) = if s == 0
@@ -29,3 +44,6 @@ normalize (Deca s e) = if s == 0
     else let (q, r) = quotRem s 10 in if r == 0
         then normalize $ Deca q (e + 1)
         else Deca s e
+
+signum :: (Num s, Num e) => Deca s e -> Deca s e
+signum (Deca s _) = Deca (Prelude.signum s) 0
--- src/test/Main.hs
+++ src/test/Main.hs
@@ -1,61 +1,59 @@
-import Deca
+import qualified Deca
 import Test.HUnit
 
 main :: IO ()
 main = runTestTTAndExit $ "Deca" ~:
-    [ "denormalize" ~:
-        [ denormalize 0 0 ~?= (Deca 0 0, Deca 0 0)
-        , denormalize 1 0 ~?= (Deca 1 0, Deca 0 0)
-        , denormalize 0 1 ~?= (Deca 0 0, Deca 1 0)
-        , denormalize 10 2 ~?= (Deca 10 0, Deca 2 0)
-        , denormalize 1 20 ~?= (Deca 1 0, Deca 20 0)
+    [ "abs" ~:
+        [ Deca.abs (Deca.deca 0 0) ~?= Deca.Deca 0 0
+        , Deca.abs (Deca.deca 1 0) ~?= Deca.Deca 1 0
+        , Deca.abs (Deca.deca (-1) 0) ~?= Deca.Deca 1 0
+        ]
+    , "add" ~:
+        [ Deca.add 1 2 ~?= Deca.Deca 3 0
+        , Deca.add 1 20 ~?= Deca.Deca 21 0
+        , Deca.add 10 2 ~?= Deca.Deca 12 0
+        , Deca.add 5 5 ~?= Deca.Deca 1 1
         ]
     , "deca" ~:
-        [ deca 0 0 ~?= Deca 0 0
+        [ Deca.deca 0 0 ~?= Deca.Deca 0 0
+        ]
+    , "denormalize" ~:
+        [ Deca.denormalize 0 0 ~?= (Deca.Deca 0 0, Deca.Deca 0 0)
+        , Deca.denormalize 1 0 ~?= (Deca.Deca 1 0, Deca.Deca 0 0)
+        , Deca.denormalize 0 1 ~?= (Deca.Deca 0 0, Deca.Deca 1 0)
+        , Deca.denormalize 10 2 ~?= (Deca.Deca 10 0, Deca.Deca 2 0)
+        , Deca.denormalize 1 20 ~?= (Deca.Deca 1 0, Deca.Deca 20 0)
+        ]
+    , "fromInteger" ~:
+        [ Deca.fromInteger 0 ~?= Deca.Deca 0 0
+        , Deca.fromInteger 1 ~?= Deca.Deca 1 0
+        , Deca.fromInteger 10 ~?= Deca.Deca 1 1
+        ]
+    , "multiply" ~:
+        [ Deca.multiply 2 3 ~?= Deca.Deca 6 0
+        , Deca.multiply 2 30 ~?= Deca.Deca 6 1
+        , Deca.multiply 20 3 ~?= Deca.Deca 6 1
+        , Deca.multiply 2 5 ~?= Deca.Deca 1 1
+        , Deca.multiply 20 30 ~?= Deca.Deca 6 2
+        ]
+    , "negate" ~:
+        [ Deca.negate (Deca.deca 0 0) ~?= Deca.Deca 0 0
+        , Deca.negate (Deca.deca 1 0) ~?= Deca.Deca (-1) 0
+        , Deca.negate (Deca.deca (-1) 0) ~?= Deca.Deca 1 0
         ]
     , "normalize" ~:
-        [ normalize (Deca 0 0) ~?= Deca 0 0
-        , normalize (Deca 0 1) ~?= Deca 0 0
-        , normalize (Deca 0 (-1)) ~?= Deca 0 0
-        , normalize (Deca 1 0) ~?= Deca 1 0
-        , normalize (Deca 10 0) ~?= Deca 1 1
-        , normalize (Deca 12 0) ~?= Deca 12 0
-        , normalize (Deca 10 1) ~?= Deca 1 2
-        , normalize (Deca 10 (-1)) ~?= Deca 1 0
+        [ Deca.normalize (Deca.Deca 0 0) ~?= Deca.Deca 0 0
+        , Deca.normalize (Deca.Deca 0 1) ~?= Deca.Deca 0 0
+        , Deca.normalize (Deca.Deca 0 (-1)) ~?= Deca.Deca 0 0
+        , Deca.normalize (Deca.Deca 1 0) ~?= Deca.Deca 1 0
+        , Deca.normalize (Deca.Deca 10 0) ~?= Deca.Deca 1 1
+        , Deca.normalize (Deca.Deca 12 0) ~?= Deca.Deca 12 0
+        , Deca.normalize (Deca.Deca 10 1) ~?= Deca.Deca 1 2
+        , Deca.normalize (Deca.Deca 10 (-1)) ~?= Deca.Deca 1 0
         ]
-    , "Num" ~:
-        [ "abs" ~:
-            [ abs (deca 0 0) ~?= Deca 0 0
-            , abs (deca 1 0) ~?= Deca 1 0
-            , abs (deca (-1) 0) ~?= Deca 1 0
-            ]
-        , "fromInteger" ~:
-            [ 0 ~?= Deca 0 0
-            , 1 ~?= Deca 1 0
-            , 10 ~?= Deca 1 1
-            ]
-        , "negate" ~:
-            [ negate (deca 0 0) ~?= Deca 0 0
-            , negate (deca 1 0) ~?= Deca (-1) 0
-            , negate (deca (-1) 0) ~?= Deca 1 0
-            ]
-        , "signum" ~:
-            [ signum (deca 0 0) ~?= Deca 0 0
-            , signum (deca 2 0) ~?= Deca 1 0
-            , signum (deca (-2) 0) ~?= Deca (-1) 0
-            ]
-        , "+" ~:
-            [ 1 + 2 ~?= Deca 3 0
-            , 1 + 20 ~?= Deca 21 0
-            , 10 + 2 ~?= Deca 12 0
-            , 5 + 5 ~?= Deca 1 1
-            ]
-        , "*" ~:
-            [ 2 * 3 ~?= Deca 6 0
-            , 2 * 30 ~?= Deca 6 1
-            , 20 * 3 ~?= Deca 6 1
-            , 2 * 5 ~?= Deca 1 1
-            , 20 * 30 ~?= Deca 6 2
-            ]
+    , "signum" ~:
+        [ Deca.signum (Deca.deca 0 0) ~?= Deca.Deca 0 0
+        , Deca.signum (Deca.deca 2 0) ~?= Deca.Deca 1 0
+        , Deca.signum (Deca.deca (-2) 0) ~?= Deca.Deca (-1) 0
         ]
     ]
