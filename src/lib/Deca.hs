01086525210b220440d5a425200c9d677a7eedd6
Taylor Fausak <taylor@fausak.me>
2021-09-25 13:16:17 +0000

    Add `Fractional` instance

--- src/lib/Deca.hs
+++ src/lib/Deca.hs
@@ -1,8 +1,14 @@
 module Deca where
 
+import qualified Data.Ratio as Ratio
+
 data Deca significand exponent
     = Deca significand exponent
     deriving (Eq, Show)
+
+instance (Integral s, Integral e) => Fractional (Deca s e) where
+    fromRational = Deca.fromRational
+    (/) = divide
 
 instance (Integral s, Num e, Ord e) => Num (Deca s e) where
     abs = Deca.abs
@@ -24,7 +30,7 @@ abs (Deca s e) = Deca (Prelude.abs s) e
 add :: (Integral s, Num e, Ord e) => Deca s e -> Deca s e -> Deca s e
 add x y =
     let (Deca s1 e1, Deca s2 _) = denormalize x y
-    in normalize $ Deca (s1 + s2) e1
+    in deca (s1 + s2) e1
 
 compare :: (Num s, Num e, Ord s, Ord e) => Deca s e -> Deca s e -> Ordering
 compare x y =
@@ -40,11 +46,33 @@ denormalize (Deca s1 e1) (Deca s2 e2) = case Prelude.compare e1 e2 of
     EQ -> (Deca s1 e1, Deca s2 e2)
     GT -> denormalize (Deca (s1 * 10) (e1 - 1)) (Deca s2 e2)
 
+divide :: (Integral s, Integral e) => Deca s e -> Deca s e -> Deca s e
+divide x y = Deca.fromRational $ Deca.toRational x / Deca.toRational y
+
+factor :: (Num a, Integral b) => b -> a -> b -> (a, b)
+factor d c n =
+    let (q, r) = quotRem n d
+    in if n /= 0 && r == 0
+    then factor d (c + 1) q
+    else (c, n)
+
 fromInteger :: (Integral s, Num e) => Integer -> Deca s e
-fromInteger s = normalize $ Deca (Prelude.fromInteger s) 0
+fromInteger s = deca (Prelude.fromInteger s) 0
+
+fromRational :: (Integral s, Integral e) => Rational -> Deca s e
+fromRational r =
+    let
+        n = Ratio.numerator r
+        d1 = Ratio.denominator r
+        (a, d2) = factor 2 0 d1
+        (b, d3) = factor 5 0 d2
+        e = max a b
+    in if d3 == 1
+    then deca (Prelude.fromInteger n * 2 ^ (e - a) * 5 ^ (e - b)) (Prelude.negate e)
+    else error $ "fromRational: " <> show r
 
 multiply :: (Integral s, Num e) => Deca s e -> Deca s e -> Deca s e
-multiply (Deca s1 e1) (Deca s2 e2) = normalize $ Deca (s1 * s2) (e1 + e2)
+multiply (Deca s1 e1) (Deca s2 e2) = deca (s1 * s2) (e1 + e2)
 
 negate :: Num s => Deca s e -> Deca s e
 negate (Deca s e) = Deca (Prelude.negate s) e
--- src/test/Main.hs
+++ src/test/Main.hs
@@ -4,9 +4,9 @@ import Test.HUnit
 main :: IO ()
 main = runTestTTAndExit $ "Deca" ~:
     [ "abs" ~:
-        [ Deca.abs (Deca.deca 0 0) ~?= Deca.Deca 0 0
-        , Deca.abs (Deca.deca 1 0) ~?= Deca.Deca 1 0
-        , Deca.abs (Deca.deca (-1) 0) ~?= Deca.Deca 1 0
+        [ Deca.abs 0 ~?= Deca.Deca 0 0
+        , Deca.abs 1 ~?= Deca.Deca 1 0
+        , Deca.abs (-1) ~?= Deca.Deca 1 0
         ]
     , "add" ~:
         [ Deca.add 1 2 ~?= Deca.Deca 3 0
@@ -31,10 +31,25 @@ main = runTestTTAndExit $ "Deca" ~:
         , Deca.denormalize 10 2 ~?= (Deca.Deca 10 0, Deca.Deca 2 0)
         , Deca.denormalize 1 20 ~?= (Deca.Deca 1 0, Deca.Deca 20 0)
         ]
+    , "divide" ~:
+        [ Deca.divide 1 1 ~?= Deca.Deca 1 0
+        , Deca.divide 1 2 ~?= Deca.Deca 5 (-1)
+        ]
     , "fromInteger" ~:
         [ Deca.fromInteger 0 ~?= Deca.Deca 0 0
         , Deca.fromInteger 1 ~?= Deca.Deca 1 0
         , Deca.fromInteger 10 ~?= Deca.Deca 1 1
+        ]
+    , "fromRational" ~:
+        [ Deca.fromRational 0 ~?= Deca.Deca 0 0
+        , Deca.fromRational 1 ~?= Deca.Deca 1 0
+        , Deca.fromRational 10 ~?= Deca.Deca 1 1
+        , Deca.fromRational (-1) ~?= Deca.Deca (-1) 0
+        , Deca.fromRational (-10) ~?= Deca.Deca (-1) 1
+        , Deca.fromRational 0.1 ~?= Deca.Deca 1 (-1)
+        , Deca.fromRational (-0.1) ~?= Deca.Deca (-1) (-1)
+        , Deca.fromRational 1.2 ~?= Deca.Deca 12 (-1)
+        , Deca.fromRational 12.34 ~?= Deca.Deca 1234 (-2)
         ]
     , "multiply" ~:
         [ Deca.multiply 2 3 ~?= Deca.Deca 6 0
@@ -44,9 +59,9 @@ main = runTestTTAndExit $ "Deca" ~:
         , Deca.multiply 20 30 ~?= Deca.Deca 6 2
         ]
     , "negate" ~:
-        [ Deca.negate (Deca.deca 0 0) ~?= Deca.Deca 0 0
-        , Deca.negate (Deca.deca 1 0) ~?= Deca.Deca (-1) 0
-        , Deca.negate (Deca.deca (-1) 0) ~?= Deca.Deca 1 0
+        [ Deca.negate 0 ~?= Deca.Deca 0 0
+        , Deca.negate 1 ~?= Deca.Deca (-1) 0
+        , Deca.negate (-1) ~?= Deca.Deca 1 0
         ]
     , "normalize" ~:
         [ Deca.normalize (Deca.Deca 0 0) ~?= Deca.Deca 0 0
@@ -59,14 +74,14 @@ main = runTestTTAndExit $ "Deca" ~:
         , Deca.normalize (Deca.Deca 10 (-1)) ~?= Deca.Deca 1 0
         ]
     , "signum" ~:
-        [ Deca.signum (Deca.deca 0 0) ~?= Deca.Deca 0 0
-        , Deca.signum (Deca.deca 2 0) ~?= Deca.Deca 1 0
-        , Deca.signum (Deca.deca (-2) 0) ~?= Deca.Deca (-1) 0
+        [ Deca.signum 0 ~?= Deca.Deca 0 0
+        , Deca.signum 2 ~?= Deca.Deca 1 0
+        , Deca.signum (-2) ~?= Deca.Deca (-1) 0
         ]
     , "toRational" ~:
-        [ Deca.toRational (Deca.deca 0 0) ~?= 0
-        , Deca.toRational (Deca.deca 1 0) ~?= 1
-        , Deca.toRational (Deca.deca 1 1) ~?= 10
-        , Deca.toRational (Deca.deca 1 (-1)) ~?= 0.1
+        [ Deca.toRational 0 ~?= 0
+        , Deca.toRational 1 ~?= 1
+        , Deca.toRational 10 ~?= 10
+        , Deca.toRational 0.1 ~?= 0.1
         ]
     ]
